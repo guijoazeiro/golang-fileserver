13670658260239aa0afa13474d4d307ba2e3d401
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2024-07-22 18:34:22 -0300

    add auth

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -5,8 +5,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	auth "github.com/abbot/go-http-auth"
 )
 
+func Secret(user, realm string) string {
+	if user == "admin" {
+		return "$1$GIFBxVQH$hNhX59hRo7cwcXUFDAw8/0"
+	}
+
+	return "secret"
+}
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: <http-dir> <port>")
@@ -15,7 +24,11 @@ func main() {
 
 	httpDir := os.Args[1]
 	port := os.Args[2]
-	fs := http.FileServer(http.Dir(httpDir))
+
+	authenticator := auth.NewBasicAuthenticator("fileserver.com", Secret)
+	http.HandleFunc("/", authenticator.Wrap(func(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+		http.FileServer(http.Dir(httpDir)).ServeHTTP(w, &r.Request)
+	}))
 	fmt.Printf("Listening on http://localhost:%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, fs))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module fileserver
 
 go 1.21.6
 
+require (
+	github.com/abbot/go-http-auth v0.4.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+)
